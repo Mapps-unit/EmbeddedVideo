7038acf9498d271f5e2bd9c7f25f47250f1cec18
humonnom <josojoso0702@gmail.com>
2021-09-08 02:14:26 +0900

    [modify EmbeddedVideo components] iframe, src

--- src/widgets/EmbeddedVideo.js
+++ src/widgets/EmbeddedVideo.js
@@ -7,7 +7,7 @@ import React from 'react';
 		  <iframe
 			width="640"
 			height="360"
-			src="https://www.youtube.com/embed/r6AKOIZOC1I"
+			src={`https://www.youtube.com/embed/${embedId}`}
 			frameBorder="0"
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 			allowFullScreen
