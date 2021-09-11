387895385a18726affa502a88daa367421dd801e
김태훈(taehoon_kim2) <taehoon_kim2@kolon.com>
2021-09-11 14:07:01 +0900

    update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,60 +1,71 @@
 import React, { useState } from "react";
 import { EmbeddedVideo } from "../widgets";
-import { PageWrapper } from "../components"
+import { PageWrapper } from "../components";
+import { css, cx } from "@emotion/css";
 
 function Main(props) {
-  const [ state, setState ] = useState("hidden");
-  const [ url, setUrl ] = useState("");
-  const [ videoIdList, setVideoIdList ] = useState([]);
-  
+  const [show, setShow] = useState(false);
+  const [url, setUrl] = useState("");
+  const [videoIdList, setVideoIdList] = useState([]);
 
-  const handleChange = ({ target: { value }}) => {
-	  	setUrl(value);
-	};
+  const handleChange = ({ target: { value } }) => {
+    setUrl(value);
+  };
 
   const handleSubmit = (event) => {
-	  event.preventDefault();
-	  let videoId = url.split('v=')[1];
-	  let position = videoId.indexOf('&');
-	  if(position !== -1) {
-		videoId = videoId.substring(0, position);
-	  }
-		setVideoIdList(videoIdList => videoIdList.concat(videoId));
-	  setUrl("");
-  }
+    try {
+      let urlObj = new URL(url);
+      setVideoIdList((videoIdList) =>
+        videoIdList.concat(urlObj.searchParams.get("v"))
+      );
+      setUrl("");
+    } catch (e) {
+      alert("이상한 url!!!");
+    }
+  };
   const EmbeddedVideoList = videoIdList.map((video, idx) => {
-	return (
-		<EmbeddedVideo embedId={video} />
-	);
+    return <EmbeddedVideo embedId={video} />;
   });
+
+  const handleKeyDown = (event) => {
+    console.log(event);
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+  const color = "white";
 
   return (
     <PageWrapper>
-
       <h3>Click button to add video widget</h3>
-      <button onClick={() =>
-		  setState("visible")
-		  }>+ video </button>
-		<p>state:{state}</p>
-      {state === "visible" ? (
-		  <form onSubmit={handleSubmit}>
-			  <input
-			  	type="url"
-			  	name="url"
-				value={url}
-				onChange={handleChange}
-				/>
-			  <button type="submit">임베드하기</button>
-		  </form>
-	  	)
-	   : null}
-	   <p>
-	   videoList:
-
-	   </p>
-	   {videoIdList}
-      	{EmbeddedVideoList}
-  </PageWrapper>
+      <button onClick={() => setShow((show) => !show)}>+ video </button>
+      <p>state:{show}</p>
+      {show && (
+        <div
+          className={css`
+            padding: 32px;
+            background-color: hotpink;
+            font-size: 24px;
+            border-radius: 4px;
+            &:hover {
+              color: ${color};
+            }
+          `}
+        >
+          <input
+            type="url"
+            name="url"
+            value={url}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <button onClick={handleSubmit}>임베드하기</button>
+        </div>
+      )}
+      <p>videoList:</p>
+      {videoIdList}
+      {EmbeddedVideoList}
+    </PageWrapper>
   );
 }
 
